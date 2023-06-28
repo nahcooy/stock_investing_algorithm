f806426979f0d26002086fafb2c8aa8b3920a3ca
nahcooy <124772010+nahcooy@users.noreply.github.com>
2023-06-29 04:00:16 +0900

    Update day2weekNmonth.py

--- 주식데이터크롤링/day2weekNmonth.py
+++ 주식데이터크롤링/day2weekNmonth.py
@@ -4,8 +4,12 @@ import Stock_Info_Crawling
 def day2weekNmonth(stock_name):
     df = Stock_Info_Crawling.save_stock_data_to_csv(stock_name)
 
+    # 날짜 정보를 datetime 형식으로 변환하여 인덱스로 설정
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+
     # 주별 주가 데이터 생성
-    weekly_data = df.resample('W', on='Date').agg({
+    weekly_data = df.resample('W').agg({
         'Open': 'first',
         'High': 'max',
         'Low': 'min',
@@ -18,7 +22,7 @@ def day2weekNmonth(stock_name):
     weekly_data.to_csv(f'{stock_name}_week.csv', index=True)
 
     # 월별 주가 데이터 생성
-    monthly_data = df.resample('M', on='Date').agg({
+    monthly_data = df.resample('M').agg({
         'Open': 'first',
         'High': 'max',
         'Low': 'min',
@@ -32,11 +36,7 @@ def day2weekNmonth(stock_name):
 
 def main():
     stock_name = input("일, 주, 월별 주가 정보를 CSV파일로 저장할 주식의 이름을 입력하세요: ")
-    df = day2weekNmonth(stock_name)
-
-    if df is not None:
-        # 주식 데이터를 활용한 그래프 그리기 등의 작업 수행
-        pass
+    day2weekNmonth(stock_name)
 
 if __name__=="__main__":
     main()
